47a2a74d992033760d1e37f914d2a1913e4a4251
Gideon Le Grange <gideon@legrange.me>
2020-06-14 19:48:07 +0200

    Split off from modbus-mqtt

---
+++ src/main/java/me/legrange/modbus/ModbusReader.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2016 gideon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.legrange.modbus;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import me.legrange.modbus.ModbusError;
+import me.legrange.modbus.ModbusException;
+import me.legrange.modbus.ReadInputRegisters;
+import me.legrange.modbus.ResponseFrame;
+import me.legrange.modbus.SerialModbusPort;
+
+/**
+ *
+ * @since 1.0
+ * @author Gideon le Grange https://github.com/GideonLeGrange
+ */
+public class ModbusReader implements Runnable {
+
+    public ModbusReader(SerialModbusPort modbus, String name, int deviceId, boolean zeroBased) throws ModbusReaderException {
+        this.name = name;
+        this.deviceId = deviceId;
+        this.zeroBased = zeroBased;
+        this.modbus = modbus;
+    }
+
+    public void setPollInterval(int interval) {
+        this.pollInterval = interval * 1000;
+    }
+
+    public void addRegister(ModbusRegister reg) {
+        registers.add(reg);
+        polls = null;
+    }
+
+    public void addListener(ModbusListener listener) {
+        listeners.add(listener);
+    }
+
+    public void start() {
+        Thread t = new Thread(this, "Modbus poller");
+        t.setDaemon(true);
+        t.start();
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        while (running) {
+            long start = System.currentTimeMillis();
+            List<Poll> polls = getPolls();
+            for (Poll poll : polls) {
+                try {
+                    int addr = (zeroBased ? poll.getAddress() - 1 : poll.getAddress());
+                    ReadInputRegisters req = new ReadInputRegisters(deviceId, addr, poll.getSize());
+                    ResponseFrame res = modbus.poll(req);
+                    for (ModbusListener l : listeners) {
+                        if (!res.isError()) {
+                            poll.applyBytes(res.getBytes(), l);
+                        } else {
+                            l.error(new ModbusReaderException(String.format("Modbus error: %s", ModbusError.valueOf(res.getFunction()))));
+                        }
+                    }
+
+                } catch (ModbusException ex) {
+                    for (ModbusListener l : listeners) {
+                        l.error(new ModbusReaderException(ex.getMessage(), ex));
+                    }
+                }
+            }
+            long stop = System.currentTimeMillis();
+            try {
+                TimeUnit.MILLISECONDS.sleep(pollInterval - (stop - start));
+            } catch (InterruptedException ex) {
+            }
+        }
+
+    }
+
+    private List<Poll> getPolls() {
+        if (polls == null) {
+            polls = Poll.generatePolls(registers);
+        }
+        return polls;
+    }
+
+    private void initModbus(String port, int speed) throws ModbusException {
+        modbus = SerialModbusPort.open(port, speed);
+    }
+
+    private boolean running;
+    private long pollInterval = 60000;
+    private String name;
+    private final int deviceId;
+    private SerialModbusPort modbus;
+    private final List<ModbusListener> listeners = new LinkedList<>();
+    private final List<ModbusRegister> registers = new LinkedList<>();
+    private List<Poll> polls = null;
+    private final boolean zeroBased;
+}
---
+++ src/main/java/me/legrange/modbus/Poll.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2016 gideon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.legrange.modbus;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A modbus poll operation. 
+ * @author gideon
+ */
+class Poll {
+ 
+    private final int address;
+    private int size;
+    private final Map<Integer, ModbusRegister> registers = new HashMap<>();
+    
+    private Poll(ModbusRegister reg) {
+        this.address = reg.getAddress();
+        this.size = reg.getLength();
+        registers.put(0, reg);
+    }
+    
+    private void add(ModbusRegister reg) { 
+        size = size + reg.getLength();
+        registers.put(reg.getAddress() - address, reg);
+    }
+    
+    int getAddress() {
+        return address;
+    }
+
+    int getSize() {
+        return size;
+    };
+    
+    /** call listener.received() for each register and it's subset of bytes in the response received 
+     * 
+     * @param bytes The bytes received for all registers polled. 
+     * @param listener The listener to call. 
+     */
+    void applyBytes(byte bytes[], ModbusListener listener) {
+        registers.keySet().stream().map((offset) -> registers.get(offset)).forEach((ModbusRegister reg) -> {
+            byte buf[] = new byte[reg.getLength()*2];
+            System.arraycopy(bytes, (reg.getAddress() - address)*2, buf, 0, reg.getLength()*2);
+            listener.received(reg, buf);
+        });
+    }
+    
+    static List<Poll> generatePolls(List<ModbusRegister> registers) {
+        Collections.sort(registers, (ModbusRegister o1, ModbusRegister o2) -> o1.getAddress() - o2.getAddress());
+        Poll poll = null;
+        List<Poll> result = new LinkedList<>();
+        for (ModbusRegister reg : registers) {
+            if ((poll != null) && ((poll.address + poll.size) == reg.getAddress())) { 
+                poll.add(reg);
+            }
+            else {
+                poll = new Poll(reg);
+                result.add(poll);
+            }
+        }
+        return result;
+    }
+    
+}
